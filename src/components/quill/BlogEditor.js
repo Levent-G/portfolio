f83368204f9270709939f7b4aa09cc719492203d
LEVENT-G <aquariiv@hotmail.com>
2024-09-04 15:14:16 +0300

    blogEditor yup sorunu çözüldü.

--- src/components/quill/BlogEditor.js
+++ src/components/quill/BlogEditor.js
@@ -1,29 +1,49 @@
 import React from "react";
 import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css"; // Quill stil dosyası
+import "react-quill/dist/quill.snow.css"; 
 import CustomTypography from "../typography/CustomTypography";
 import { Controller } from "react-hook-form";
+import { Typography } from "@mui/material"; 
+import { Box } from "@gib-ui/core";
 
 const BlogEditor = ({ control, name, ...props }) => {
   return (
-    <>
-      <CustomTypography variant="h7" text={props?.label} />
-
+    <Box className="mb-20">
       <Controller
         name={name}
         control={control}
-        render={({ field }) => (
-          <ReactQuill
-            value={field.value || ""}
-            onChange={field.onChange}
-            modules={BlogEditor.modules}
-            formats={BlogEditor.formats}
-            theme="snow"
-            className="h-[200px] mb-20"
-          />
+        render={({
+          field,
+          fieldState: { error }, 
+        }) => (
+          <>
+            <CustomTypography
+              variant="h7"
+              text={props?.label}
+              className={error ? "text-red-500" : ""}
+            />
+         
+              <ReactQuill
+                value={field.value || ""}
+                onChange={field.onChange}
+                modules={BlogEditor.modules}
+                formats={BlogEditor.formats}
+                theme="snow"
+                className={`h-[200px] mb-12    ${
+                  error ? " border border-red-500" : ""
+                }`} 
+              />
+           
+            {error && (
+              <Typography variant="body2" color="error">
+                {error.message}
+              </Typography>
+            )}{" "}
+           
+          </>
         )}
       />
-    </>
+    </Box>
   );
 };
 
