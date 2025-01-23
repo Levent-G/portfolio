68b5d247cb3a37d44e5893cca817866166a4e8b1
LEVENT-G <aquariiv@hotmail.com>
2025-01-23 17:08:34 +0300

    test

--- src/pages/Deneme.jsx
+++ src/pages/Deneme.jsx
@@ -2,21 +2,11 @@ import React, { useState } from 'react';
 import './deneme.css';
 
 const Deneme = () => {
-  const [moveButton, setMoveButton] = useState(false);
   const [explosion, setExplosion] = useState(false);
-  const [hasMoved, setHasMoved] = useState(false); // Butonun hareket edip etmediğini takip etmek için
 
-  const handleMouseEnter = () => {
-    // Eğer buton daha önce hareket etmediyse, hareket etmesini sağlıyoruz
-    if (!hasMoved) {
-      setMoveButton(true);
-      setHasMoved(true); // Buton bir kere hareket etti
-    }
-  };
+ 
 
-  const handleMouseLeave = () => {
-    setMoveButton(false);
-  };
+
 
   const handleClickEvet = () => {
     setExplosion(true);
