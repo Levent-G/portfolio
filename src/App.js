c73df3a11097c883036591f7db09a56997f2c447
LEVENT-G <aquariiv@hotmail.com>
2025-05-01 11:20:12 +0300

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import { useState, useEffect } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { auth } from "./firebase/firebase"; 
-import { onAuthStateChanged } from "firebase/auth";
+import { BrowserRouter, Routes, Route,  } from "react-router-dom";
 
-import Nav from "./layouts/Nav";
 import Footer from "./layouts/Footer";
 import PdfPages from "./components/pdf/PdfPages";
-import LoadingPage from "./layouts/LoadingPage";
 import Homepage from "./pages/homePage/Homepage";
 import BlogContent from "./pages/blog/BlogContent";
 import Blog from "./pages/blog/Blog";
 import { ThemeProvider } from "./context/ThemeContext";
-import ColorPickerComponent from "./components/colorPicker/ColorPickerComponent";
 import BlogEkleMain from "./pages/blog/blogEkle/BlogEkleMain";
-import OurDreams from "./pages/deneme/OurDreams";
-import Login from "./pages/deneme/Login/Login";
 
 function App() {
 
