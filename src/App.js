1e490adffdfa665b371f2f95d8deeee7da6ecb3c
LEVENT-G <aquariiv@hotmail.com>
2025-05-01 11:19:48 +0300

    aaa

--- src/App.js
+++ src/App.js
@@ -17,43 +17,23 @@ import OurDreams from "./pages/deneme/OurDreams";
 import Login from "./pages/deneme/Login/Login";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-      setUser(currentUser);
-    });
-    return () => unsubscribe();
-  }, []);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000);
-  }, []);
-
-  const ProtectedRoute = ({ children }) => {
-    return user ? children : <Navigate to="/ourdreams/login" />;
-  };
 
   return (
     <div className="App">
       <ThemeProvider>
-        {window.location.pathname !== "/ourdreams" && window.location.pathname !== "/ourdreams/login" && loading && <LoadingPage />}
+    
         <BrowserRouter>
-          {window.location.pathname !== "/ourdreams" && window.location.pathname !== "/ourdreams/login" && <Nav />}
-          {window.location.pathname !== "/ourdreams" && window.location.pathname !== "/ourdreams/login" && <ColorPickerComponent />}
+   
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blogcontent/:bloguuid" element={<BlogContent />} />
             <Route path="/pdf" element={<PdfPages />} />
             <Route path="/blogekle/:blogerName" element={<BlogEkleMain />} />
-            <Route path="/ourdreams/login" element={<Login />} />
-            <Route path="/ourdreams" element={<ProtectedRoute><OurDreams /></ProtectedRoute>} />
+        
           </Routes>
-          {window.location.pathname !== "/ourdreams" && <Footer />}
+         <Footer />
         </BrowserRouter>
       </ThemeProvider>
     </div>
