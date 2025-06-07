12ee16c8ddb5326a0c44b567e1dbe11ad15ed3dc
LEVENT-G <aquariiv@hotmail.com>
2025-06-07 13:42:35 +0300

    nav eklendi.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route,  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Footer from "./layouts/Footer";
 import PdfPages from "./components/pdf/PdfPages";
@@ -7,25 +7,22 @@ import BlogContent from "./pages/blog/BlogContent";
 import Blog from "./pages/blog/Blog";
 import { ThemeProvider } from "./context/ThemeContext";
 import BlogEkleMain from "./pages/blog/blogEkle/BlogEkleMain";
+import TopBar from "./layouts/Nav";
 
 function App() {
-
-
   return (
     <div className="App">
       <ThemeProvider>
-    
         <BrowserRouter>
-   
+          <TopBar />
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/blogcontent/:bloguuid" element={<BlogContent />} />
             <Route path="/pdf" element={<PdfPages />} />
             <Route path="/blogekle/:blogerName" element={<BlogEkleMain />} />
-        
           </Routes>
-         <Footer />
+          <Footer />
         </BrowserRouter>
       </ThemeProvider>
     </div>
